a749e0f8b70eb57b573eec90bcf67b7f57f32bc4
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-06-30 19:56:47 -0400

    updated documentation

--- constants/files.py
+++ constants/files.py
@@ -1,2 +1,10 @@
-INTERACTION_DATA = ['./mdh_files/interaction_data_s24.csv', './mdh_files/interaction_data_f23.csv', './mdh_files/interaction_data_s23.csv']
-SHIFT_DATA = ['./mdh_files/shift_data_s24.csv', './mdh_files/shift_data_f23.csv', './mdh_files/shift_data_s23.csv']
+INTERACTION_DATA = [
+    './mdh_files/interaction_data_s24.csv',
+    './mdh_files/interaction_data_f23.csv',
+    './mdh_files/interaction_data_s23.csv'
+]
+SHIFT_DATA = [
+    './mdh_files/shift_data_s24.csv',
+    './mdh_files/shift_data_f23.csv',
+    './mdh_files/shift_data_s23.csv'
+]
